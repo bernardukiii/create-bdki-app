c91a8ef3c66a28ffe2e842732ea08985566db522
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-01-14 21:04:12 -0300

    Style: correcting Header glassify
    As the header is on the layout, I need its background to be transparent to properly notice the glassify

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -20,7 +20,7 @@ const Header = () => {
 
     return (
         <>
-            <header className="sticky top-0 w-full z-10 bg-bdki-darkest flex justify-center items-center text-white font-semibold text-sm md:text-base lg:text-base border-b-2 border-b-bdki-grey bdki-glassify">
+            <header className="sticky top-0 w-full z-10 flex justify-center items-center text-white font-semibold text-sm md:text-base lg:text-base border-b-2 border-b-bdki-grey bdki-glassify">
                 <div className="flex justify-center items-center w-1/3">
                     <button className="flex-grow p-2 hover:text-bdki-lighter-grey focus:text-lighter-grey bdki-cursor">ITEM 1</button>
                     <button className="flex-grow p-2 hover:text-bdki-lighter-grey focus:text-lighter-grey bdki-cursor">ITEM 2</button>
